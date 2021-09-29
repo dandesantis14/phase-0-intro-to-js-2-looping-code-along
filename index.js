3210bfd628eba1aa0f805ecc44cb36a609b0a7f7
Daniel <dandesantis14@gmail.com>
2021-09-29 17:28:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names,event){
+    let thanks = [];
+    for (let i = 0; i < names.length; i++){
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanks;
+}
+
+function countDown(num){
+    let count = num;
+    while (count >= 0){
+        console.log(count--);
+    }
+}
